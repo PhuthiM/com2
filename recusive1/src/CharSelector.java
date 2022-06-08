d3117cb06af83b69591a5f1ab0f796a8beed813d
Smile KID <90183035+PhuthiM@users.noreply.github.com>
2022-06-08 23:34:51 +0700

    Add files via upload

---
+++ recusive1/src/CharSelector.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+
+public class CharSelector {
+
+    String selectChar(String s, int[] k) {
+        // Enter your code here and only here
+        int n = s.length(); // ความยาวสตริง
+        if (n <= 0) {
+            return "";
+        }
+        int ad = k[n - 1];  // ค่าของตำแหน่งเดิม
+        int result = n - ad - 1; // ตำแหน่งของค่าที่เปลียน
+        //   System.out.println("N " + n + " AD " + ad + " RE " + result);
+        if (result <= 0) {
+            result = n - (ad % n) - 1;
+            //  System.out.println("TT" + result);
+        }
+        // System.out.println(s.charAt(result));
+        return selectChar(s.substring(0, n - 1), k) + s.charAt(result);
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        final int N = s.length();
+        int[] k = new int[N];
+        for (int i = 0; i < N; ++i) {
+            k[i] = scan.nextInt();
+        }
+        CharSelector selector = new CharSelector();
+        String ans = selector.selectChar(s, k);
+        System.out.println(ans);
+    }
+}
+/*
+HIKARU
+3 7 1 5 2 4
+
+*/
---
+++ recusive1/src/CutHalfRecursive.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+public class CutHalfRecursive {
+
+    public static String cutHalf(String s) {
+        //your code here
+        int n = s.length() / 2;
+        if (s.length() <= 1) {
+            return s ;
+        }
+        return s.charAt(n) + cutHalf(s.substring(0, n)) + cutHalf(s.substring(n + 1));
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        System.out.println(cutHalf(str));
+    }
+}
+/*
+ABCDEFGHIJKLMNOP
+ +""+ cutHalf(s.substring(n , s.length() - 1)
+ */
